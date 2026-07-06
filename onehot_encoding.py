02be18f5822e43892d93085a01bbc710410c002e
yahya <yahyaparayil73@gmail.com>
2026-07-05 22:05:32 -0300

    learned onehotencoding

---
+++ onehot_encoding.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_excel('datasets/restaurant_dataset_100_rows.xlsx')
+
+# df =  pd.get_dummies(df,columns=['listed_in'])
+df = df.drop('name',axis=1)
+
+for col in df.columns:
+
+    if df[col].dtype == "str":
+
+        print("Encoding:", col)
+
+        df = pd.get_dummies(df, columns=[col])
+
+print(df.head())
+
+# print(df.dtypes)
