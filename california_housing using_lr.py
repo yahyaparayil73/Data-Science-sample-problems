86321cec4be3bffc931b053f4e081061c2387e1c
yahya <yahyaparayil73@gmail.com>
2026-07-10 20:33:59 -0300

    after california housing dataset workout

--- california_housing using_lr.py
+++ california_housing using_lr.py
@@ -1,45 +1,47 @@
-import numpy as np
-import pandas as pd
-from  sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error
+    import numpy as np
+    import pandas as pd
+    from  sklearn.datasets import fetch_california_housing
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
+    from sklearn.metrics import r2_score, mean_squared_error
 
-import matplotlib as plt
+    import matplotlib as plt
 
 
-data = fetch_california_housing()
-df = pd.DataFrame(data.data,columns=data.feature_names)
-# print(df)
+    data = fetch_california_housing()
+    df = pd.DataFrame(data.data,columns=data.feature_names)
+    # print(df)
 
-# print(df.isnull().sum())
+    # print(df.isnull().sum())
 
-x = df
-df["HouseValue"] = data.target
-x = df.drop("HouseValue", axis=1)
-y = df["HouseValue"]
+    x = df
+    df["HouseValue"] = data.target
+    x = df.drop("HouseValue", axis=1)
+    y = df["HouseValue"]
 
-# print(y)
-# print(x)
+    # print(y)
+    # print(x)
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-lr = LinearRegression()
+    lr = LinearRegression()
 
-lr.fit(x_train,y_train)
+    lr.fit(x_train,y_train)
 
-y_pred = lr.predict(x_test)
+    y_pred = lr.predict(x_test)
 
-print("R2 Score:", r2_score(y_test, y_pred))
-print("MSE:", mean_squared_error(y_test, y_pred))
+    print("R2 Score:", r2_score(y_test, y_pred))
+    print("MSE:", mean_squared_error(y_test, y_pred))
 
-# Predict one house
-sample = x_test.iloc[[0]]
+    # Predict one house
+    sample = x_test.iloc[[0]]
 
-prediction = lr.predict(sample)
+    prediction = lr.predict(sample)
 
-print("Predicted Value:", prediction[0])
-print("Actual Value:", y_test.iloc[0])
+    print("Predicted Value:", prediction[0])
+    print("Actual Value:", y_test.iloc[0])
+
+
 
 
 
--- standardscaler.py
+++ standardscaler.py
@@ -2,24 +2,55 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
-
-
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 data = fetch_california_housing()
 df = pd.DataFrame(data.data,columns=data.feature_names)
-print(df)
+# print(df)
+
+
 
 scaler = StandardScaler()
 
 scaled_data = scaler.fit_transform(df)
 
-print(scaled_data)
+# print(scaled_data)
 
 
-scaled_df = pd.DataFrame(scaled_data,columns = df.columns)
+scaled_df = pd.DataFrame(scaled_data,columns = df.columns)                      
 
-print(scaled_df)
+# print(scaled_df)
+
+x = df
+
+
+# print(data.target)
+
+df['housevalue'] = data.target
+
+# print(df['housevalue'])
+
+x = df.drop('housevalue',axis = 1)
+y = df['housevalue']
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+model = LinearRegression()
+
+model.fit(x_train,y_train)
+y_pred = model.predict(x_test)
+
+print(y_pred)
+
+
+
+
+
+
+
 
 
 
