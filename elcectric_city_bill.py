3f439525afbc3c9868cf02b6a5bd4d12d90d53a1
yahya <yahyaparayil73@gmail.com>
2026-07-26 07:19:41 -0300

    electricity bill generator

---
+++ elcectric_city_bill.py
@@ -0,0 +1,8 @@
+# Electricity Bill Calculator
+
+print("Enter the units consumed:")
+unit = float(input())   # Read input from the user
+
+bill = unit * 6.5       # ₹6.5 per unit
+
+print("Total Bill =", bill)
