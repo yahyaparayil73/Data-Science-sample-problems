66c9e0a8dd9864ee8b66e7fb7df07f5d4e807f0d
yahya <yahyaparayil73@gmail.com>
2026-07-29 18:26:41 -0300

    amstrong number

--- python _Core/armstrong number.py
+++ python _Core/armstrong number.py
@@ -1,2 +1,17 @@
 num = int(input('Enter the number '))
+n = num
+sum = 0
+
+while num > 0 :
+    temp = num%10
+    sum = sum + temp * temp * temp
+    num = num // 10
+
+if n == sum :
+    print('amstrong')
+else :
+    print('not amstrong')
+
+
+
 
---
+++ python _Core/pattern_printing.py
@@ -0,0 +1,4 @@
+n = 5
+
+for i in range(1,n+1):
+    
