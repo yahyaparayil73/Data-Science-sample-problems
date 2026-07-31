27906d28813a196784d0d747934e73087717c5ff
yahya <yahyaparayil73@gmail.com>
2026-07-31 10:42:20 -0300

    pattern printing

--- python _Core/pattern_printing.py
+++ python _Core/pattern_printing.py
@@ -5,9 +5,9 @@ n = 5
 #         print('* ',end ="")
 #     print()
 
-count=0
 for i in range(1,n+1):
-    for j in range(1,n-i):
-        print('* ',end ="")
-        count+=1
+    for j in range(1,n+1-i):
+        print('& ',end ="")
+    for k in range(1,i):
+        print('* ',end="")
     print()
