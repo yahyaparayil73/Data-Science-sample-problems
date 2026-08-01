3ca9c48a5b02a0a84a42e2cdf05cb148f8b33536
yahya <yahyaparayil73@gmail.com>
2026-08-01 08:30:03 -0300

    array unique elements

--- python _Core/unique_elements.py
+++ python _Core/unique_elements.py
@@ -5,10 +5,12 @@ flag = 0
 for i in range(0,len(arr)):
     flag = 0
     for j in range(len(arr)):
+        if i == j:
+            continue
         if arr[i] == arr[j]:
             flag = 1
             break
     if flag == 0:
-        arr2 = arr[i]
+        arr2.append(arr[i])
 
 print('unique elements are',arr2)
