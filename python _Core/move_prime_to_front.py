9b4202a50f14673fbc031d8e7da36f9fba66e230
yahya <yahyaparayil73@gmail.com>
2026-08-01 21:29:43 -0300

    move prime to front

--- python _Core/move_prime_to_front.py
+++ python _Core/move_prime_to_front.py
@@ -1,17 +1,17 @@
 a = [4, 7, 10, 3, 8, 5]
 n = len(a)
-count = 0
+pos = 0
 
 
 for i in range(len(a)):
+    flag = 0
     for j in range(2,a[i]):
-        flag = 0
         if a[i] % j == 0:
             flag = 1
             break
     if flag == 0:
-        count+=1   
-        a[i-count] = a[i]
+        pos+=1   
+        a[i-pos] = a[i]
 print(a)
 
 
