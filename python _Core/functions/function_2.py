19f3e13b657c982c1ed27244ba743a1c0a9f4058
yahya <yahyaparayil73@gmail.com>
2026-08-02 19:46:46 -0300

    random display from array

---
+++ python _Core/functions/function_2.py
@@ -0,0 +1,13 @@
+greeting_arr = ['hi', 'hello', 'whatsup?', 'hey', 'hey how u doing?']
+
+i = 0
+
+while True:
+    print(greeting_arr[i])
+
+    i += 1
+
+    if i == len(greeting_arr):
+        i = 0
+
+    
--- python _Core/functions/lambda.py
+++ python _Core/functions/lambda.py
@@ -4,7 +4,4 @@ add = lambda a,b:a+b
 
 print(add(a,b))
 
-add = lambda a,b:a+b
-
-print(add(a,b))
 
