e7095e664b23b1dd15a521e959d4b78c1a7cf0e4
yahya <yahyaparayil73@gmail.com>
2026-08-10 11:12:27 -0300

    habit tracker

---
+++ python _Core/mini projects/habit_tracker.py
@@ -0,0 +1,20 @@
+# ========================================
+#        PERSONAL GROWTH TRACKER
+# ========================================
+
+# Welcome, Yahya!
+
+# 1. Add Habit
+# 2. View Habits
+# 3. Complete Habit
+# 4. View Today's Progress
+# 5. View Statistics
+# 6. Search / Filter Habits
+# 7. Generate Report
+# 8. Exit
+
+# Enter your choice:
+
+
+
+print('Enter Your Choice : ')
