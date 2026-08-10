fd00d151ef4da544c3d5efb8fc7c9f20ccd3e191
yahya <yahyaparayil73@gmail.com>
2026-08-10 11:41:47 -0300

    habit tracker

--- python _Core/mini projects/habit_tracker.py
+++ python _Core/mini projects/habit_tracker.py
@@ -15,6 +15,48 @@
 
 # Enter your choice:
 
+def habits_display ():
+    print()
+    print('========HABIT TRACKER========')
+    print()
+    count = 1
+    for habit in habits:
+        print(f'{count} . {habit}')
+        count +=1
+    print()
 
 
-print('Enter Your Choice : ')
+habits = ['Workout','Meditation','Prayer','Quran','Reading','Writing','Walking','English']
+
+
+while True:
+    print("""
+    1. Add Habit
+    2. View Habits
+    3. Complete Habit
+    4. View Today's Progress
+    5. View Statistics
+    6. Search / Filter Habits
+    7. Generate Report
+    8. Exit
+    """)
+
+    choice = int(input('Enter Your Choice : '))
+
+    if choice == 1:
+        habits_display ()
+        new_habit = input('Enter the habit to be added :')
+        habits =habits.append(new_habit)
+
+        print('Here is your updated Habit Tracker :')
+        habits_display ()
+
+    if choice == 8 :
+        break
+
+
+
+
+
+
+
