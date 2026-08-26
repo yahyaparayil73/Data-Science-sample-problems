070c8a9100c0c0bff97a2605cefa76d456d82f3a
yahya <yahyaparayil73@gmail.com>
2026-08-26 11:05:55 -0300

    oops basics2

--- python _Core/functions/oops_basics_2.py
+++ python _Core/functions/oops_basics_2.py
@@ -1,8 +1,21 @@
 class hikma:
-    def employee(self,name,age,occupation):
-        print('the employee details')
 
-employee = hikma()
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+    def display(self):
+        print('The employee details is given below :')
+        print(f'The Employee name is {self.name}')
+        print(f'Monthly salary is {self.salary}')
+        print(f'Annual Salary is {self.salary*12}')
 
-yahya.employee()
+    def salary_increment(self):
+            print(f'The present salary is {self.salary}')
+            new_salary = int(self.salary + self.salary*0.3)
+            print(f'New incremented Salary is {new_salary}')
+
+employee = hikma('Yahya',25000)
+
+# employee.display()
+employee.salary_increment()
     
