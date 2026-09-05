3049ad71fbad78c94ff7760ce37a74d88762f4de
yahya <yahyaparayil73@gmail.com>
2026-09-05 11:04:31 -0300

    abstraction

---
+++ python _Core/functions/abstraction.py
@@ -0,0 +1,18 @@
+class Hikma():
+        def name(self):
+            print('Hi the name is yahya')
+
+
+syed = Hikma()
+
+syed.name()
+
+
+
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+
+#     @abstractmethod
+#     def make_sound(self):
+#         pass
